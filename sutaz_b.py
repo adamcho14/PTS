c272960ab72d259cb8522e1a974362fe44c5cd7a
adamcho14 <adamcho14@gmail.com>
2017-03-13 18:42:54 +0100

    Vypis hlasok v programe

--- sutaz_b.py
+++ sutaz_b.py
@@ -1,11 +1,12 @@
-sutaz = {} #udrziava zoznam a body
-juniors = {} #oznacenie, ci je junior
+sutaz = {}  # udrziava zoznam a body
+juniors = {}  # oznacenie, ci je junior
 
-#nacitame vstup
+# nacitame vstup
 pwd = input("Zadajte heslo: ")
 print("Vase heslo je \'" + pwd + "\'.")
 
-#dekorator, ktory si vyziada a overi heslo pre uskutocnenim prikazu
+
+# dekorator, ktory si vyziada a overi heslo pre uskutocnenim prikazu
 def password(f):
     global pwd
 
@@ -16,6 +17,7 @@ def password(f):
             print("Zle heslo. Zadajte prikaz znova:")
     return inner_func
 
+
 #funkcie zo zadania. viac v README
 @password
 def points(name, number):
@@ -23,38 +25,51 @@ def points(name, number):
         sutaz[name] += number
     else:
         sutaz[name] = number
-        juniors[name] = 0
+        juniors[name] = False
+    print("Hrac", name, "s poctom bodov", str(sutaz[name] + "."))
+
 
 @password
 def reduce(number):
     for name in sutaz:
         sutaz[name] -= number
+    print("Vsetkym hracom odobrate", number, "bodov.")
+
 
 @password
 def junior(name):
     if name in juniors:
         juniors[name] = True
+    print("Hrac", name, "oznaceny ako junior.")
+
 
 def ranking():
     por = 1
     a = sorted(sutaz.keys(), key=sutaz.__getitem__, reverse=True)
+    print("Poradie vsetkych hracov:")
     for i in a:
         print(str(por)+".", i, sutaz[i])
         por+=1
 
+
 def ranking_junior():
     por = 1
     a = sorted(sutaz.keys(), key=sutaz.__getitem__, reverse=True)
+    print("Poradie juniorov:")
     for i in a:
         if juniors[i]:
             print(str(por)+".", i, sutaz[i])
             por += 1
+
+
 @password
 def quit():
+    print("Program ukonceny.")
     exit(0)
 
-#citanie a vykonavanie jednotlivych prikazov podla vstupu
-while(True):
+
+# citanie a vykonavanie jednotlivych prikazov podla vstupu
+while True:
     do = input().split(" ")
     if do[0] == 'points':
         points(do[1], int(do[2]))
